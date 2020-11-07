e0777137242d75edeb21abd51010560eed76ec36
Elizabeth Brandt <ebrandt8@uw.edu>
2020-11-06 20:19:28 -0800

    fixed the addClass

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,22 +5,40 @@ $("#currentDay").text(time.format("dddd, MMMM Do"));
 
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-var scheduledHour = [9, 10, 11, 12, 13, 15, 16, 17];
+var currentHour = time.hour();
 
-for (var i = 0; i < scheduledHour.length; i++) {
-    
-    var currentHour = time.hour();
+for (var i = 9; i <= 22; i++) {
 
-    if (currentHour === scheduledHour[i]) {
-        $("textarea").addClass("present")
-    } else if (currentHour < scheduledHour[i]) {
-        $("textarea").addClass("future")
-    } else (currentHour > scheduledHour[i])
-        $("textarea").addClass("past")
+    if (currentHour === i) {
+        $(("#text" + i).toString()).addClass("present");
+    } else if (currentHour < i) {
+        $(("#text" + i).toString()).addClass("future");
+    } else (currentHour > i)
+        $(("#text" + i).toString()).addClass("past");
 }
+
+console.log(currentHour);
 // WHEN I click into a timeblock
 // THEN I can enter an event
+
 // WHEN I click the save button for that timeblock
+    // ADD .on("click")
+var saveButton = $("saveBtn");
+var eventInput = $("#text9");
+
+console.log(eventInput);
+
+saveButton.on("click", function () {
 // THEN the text for that event is saved in local storage
+    // SET eventInput
+    for (var i = 9; i <= 20; i++) {
+        localStorage.setItem(eventInput.val());
+    }
+})
+
+
 // WHEN I refresh the page
 // THEN the saved events persist
+    // GET eventInput
+
+
